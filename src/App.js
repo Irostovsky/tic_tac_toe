fce72bfa284dbe451bba9bd9f6b264f24f1d939f
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2023-08-22 19:44:06 +0300

    Leave in the Game only history state.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,9 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
 
   const squares = history.slice(-1)[0];
-  const winner = calculateWinner(squares);
   const nextPlayer = history.length % 2 == 0 ? "O" : "X";
 
-  let status;
-  if (winner) {
-    status = `Winner is: ${winner}`;
-  } else {
-    status = `Next player is: ${nextPlayer}`;
-  }
-
-  function handlePlay(i) {
-    if (squares[i] || winner) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    nextSquares[i] = nextPlayer;
+  function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
   }
 
@@ -28,7 +15,7 @@ export default function Game() {
       <div className="game-board">
         <Board
           squares={history.slice(-1)[0]}
-          status={status}
+          nextPlayer={nextPlayer}
           onPlay={handlePlay}
         />
       </div>
@@ -47,24 +34,42 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ squares, status, onPlay }) {
+function Board({ squares, nextPlayer, onPlay }) {
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = `Winner is: ${winner}`;
+  } else {
+    status = `Next player is: ${nextPlayer}`;
+  }
+
+  function handleClick(i) {
+    if (squares[i] || winner) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = nextPlayer;
+    onPlay(nextSquares);
+  }
+
   return (
     <React.Fragment>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => onPlay(0)} />
-        <Square value={squares[1]} onSquareClick={() => onPlay(1)} />
-        <Square value={squares[2]} onSquareClick={() => onPlay(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => onPlay(3)} />
-        <Square value={squares[4]} onSquareClick={() => onPlay(4)} />
-        <Square value={squares[5]} onSquareClick={() => onPlay(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => onPlay(6)} />
-        <Square value={squares[7]} onSquareClick={() => onPlay(7)} />
-        <Square value={squares[8]} onSquareClick={() => onPlay(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </React.Fragment>
   );
